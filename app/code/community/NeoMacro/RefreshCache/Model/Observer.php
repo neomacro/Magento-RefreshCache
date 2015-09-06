6ecfc6e5bb2d310a004d7f1e86938c571cf78e7b
Alex Takayama <alex.takayama@gmail.com>
2015-09-06 16:32:17 -0300

    Initial commit

---
+++ app/code/community/NeoMacro/RefreshCache/Model/Observer.php
@@ -0,0 +1,23 @@
+<?php
+
+class NeoMacro_RefreshCache_Model_Observer extends Mage_Core_Model_Abstract
+{
+    private static $singleton = false;
+
+    public function refreshCache($observer)
+    {
+        if (static::$singleton)
+            return;
+
+        static::$singleton = $this;
+
+        try {
+            $allTypes = Mage::app()->useCache();
+            foreach ($allTypes as $type => $value) {
+                Mage::app()->getCacheInstance()->cleanType($type);
+            }
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
+}
